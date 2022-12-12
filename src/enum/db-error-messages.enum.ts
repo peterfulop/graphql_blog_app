b37182968d45c113ae7162b0c687c16f7cb4a4a5
peterfulop <peterfulop@codingsans.com>
2022-12-12 18:52:01 +0100

    feat(db-error-messages.enum.ts): signup use-case db errors added;

--- src/enum/db-error-messages.enum.ts
+++ src/enum/db-error-messages.enum.ts
@@ -3,4 +3,9 @@ export enum DBErrorMessages {
   ONE_FIELD_TO_UPDATE = 'Need to have at least one filed to update',
   MISSING_POST = 'Selected post does not exists!',
   SERVER_ERROR = 'Something went wrong!',
+  MISSING_SIGNUP_DATA = 'You must provide a name, email, password and bio to create a user!',
+  SHORT_PASSWORD = 'Your password must be longer than 6 characters!',
+  PASSWORDS_DO_NOT_MATCH = 'Your passwords do not match!',
+  INVALID_EMAIL_ADDRESS = 'Your email address is invalid!',
+  EMAIL_ADDRESS_ALREADY_IN_USE = 'The email address is already in use! Please, choose another one!',
 }
